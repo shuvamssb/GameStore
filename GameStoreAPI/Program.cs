994741d991bc2ed3d72658c7fb69083d4b68b208
shuvamssb <shuvamsuklabaidya@gmail.com>
2024-07-14 12:02:15 +0530

    13-July-24

--- GameStoreAPI/Program.cs
+++ GameStoreAPI/Program.cs
@@ -4,35 +4,38 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
+const string GetGameEndpointName= "GetGame";
+
 List<GameDto> games =[
 new(1,
-"Genshin Impact",
-"RPG",
-109.99M,
-new DateOnly(2020,10,28)),
-new(2,
-"RDR2",
-"Action",
-1500.01M,
-new DateOnly(2017,2,28)),
-new(1,
-"FIFA 23",
-"Sports",
-55.99M,
-new DateOnly(2023,4,23))
+        "Genshin Impact",
+        "RPG",
+        109.99M,
+        new DateOnly(2020,10,28)),
+        new(2,
+        "RDR2",
+        "Action",
+        1500.01M,
+        new DateOnly(2017,2,28)),
+        new(1,
+        "FIFA 23",
+        "Sports",
+        55.99M,
+        new DateOnly(2023,4,23))
 ];
 
 
 //GET /games
 app.MapGet("games",() =>games);
 
-//GET /games
+//GET /games/1
 app.MapGet("games/{id}",(int id) => games.Find(game =>game.Id == id))
-.WithName("GetGame");
+.WithName(GetGameEndpointName); // withname is used to defined an endpoint
 
 //POST /games
 app.MapPost("games",(CreateGameDto newGame) =>
 {
+    //Add game to GameDto so game variable has been initialised
     GameDto game= new (
         games.Count+1,
         newGame.Name,
@@ -43,8 +46,9 @@ app.MapPost("games",(CreateGameDto newGame) =>
 
     games.Add(game);
 
-    return Results.CreatedAtRoute("GetGame",new {id = game.Id},game);
-    //Test
+    //Return whether the game data has been created or not
+    return Results.CreatedAtRoute(GetGameEndpointName,new {id = game.Id},game);
+    
 });
 
 app.Run();
